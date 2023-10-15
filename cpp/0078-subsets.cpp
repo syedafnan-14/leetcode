3a8cce20eff4ce99d0f4fe24422347f6b7cd805d
SYED AFNAN <84911390+syedafnan-14@users.noreply.github.com>
2023-10-15 17:31:09 +0530

    Update 0078-subsets.cpp
    Similar to your approach in c++ without using any loop

--- cpp/0078-subsets.cpp
+++ cpp/0078-subsets.cpp
@@ -9,20 +9,22 @@
 */
 
 class Solution {
+private:
+        vector<vector<int>> res;
+        void subsets(vector<int>& nums,vector<int>& temp,int i){
+            if(i==nums.size()){
+                res.push_back(temp);
+                return;
+            }
+            temp.push_back(nums[i]);
+            subsets(nums,temp,i+1);
+            temp.pop_back();
+            subsets(nums,temp,i+1);
+        }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int> curr;
-        vector<vector<int>> result;
-        dfs(nums, 0, curr, result);
-        return result;
-    }
-private:
-    void dfs(vector<int>& nums, int start, vector<int>& curr, vector<vector<int>>& result) {
-        result.push_back(curr);
-        for (int i = start; i < nums.size(); i++) {
-            curr.push_back(nums[i]);
-            dfs(nums, i + 1, curr, result);
-            curr.pop_back();
-        }
+        vector<int> temp;
+        subsets(nums,temp,0);
+        return res;
     }
 };
